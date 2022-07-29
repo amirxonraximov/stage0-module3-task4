1e56fefe5d48204fddb68f1b19f731510fcdf3af
Amirxon Raximov <arm19980201@gmail.com>
2022-07-30 00:45:57 +0500

    solvw

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+
+        int i = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
